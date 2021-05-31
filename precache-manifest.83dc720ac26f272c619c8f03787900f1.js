1ed68a8718a6bbf6133433ba01dfd8337ef03faf
Pratik Kulkarni <pratikkulkarni1307@gmail.com>
2021-05-31 13:50:44 +0530

    Updates

---
+++ precache-manifest.83dc720ac26f272c619c8f03787900f1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06c695d77271e478df04abe64f77472c",
+    "url": "/mars-weather/index.html"
+  },
+  {
+    "revision": "0997fa5c524b6577fcd8",
+    "url": "/mars-weather/static/css/main.0a27b8b2.chunk.css"
+  },
+  {
+    "revision": "431e7b76fd22e3151665",
+    "url": "/mars-weather/static/js/2.e36f57c2.chunk.js"
+  },
+  {
+    "revision": "0a572ad323474bafc6a871b6336141bd",
+    "url": "/mars-weather/static/js/2.e36f57c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0997fa5c524b6577fcd8",
+    "url": "/mars-weather/static/js/main.8cd3b3a4.chunk.js"
+  },
+  {
+    "revision": "2a95eeb6b08f4635ea03",
+    "url": "/mars-weather/static/js/runtime-main.48642b31.js"
+  },
+  {
+    "revision": "128a656a26611adc227ae02d97ad2eaa",
+    "url": "/mars-weather/static/media/blob.128a656a.svg"
+  },
+  {
+    "revision": "7c677400fd91adfd4e122bc6599e525d",
+    "url": "/mars-weather/static/media/github-icon.7c677400.svg"
+  },
+  {
+    "revision": "8d6059572f4f0edfb3d232d69abb15e2",
+    "url": "/mars-weather/static/media/mars.8d605957.svg"
+  }
+]);
